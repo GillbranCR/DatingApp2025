e0dd517ad743b87fe44047c25f049c9e9a364770
GillbranCR <gill.cadengo1@gmail.com>
2025-09-03 07:52:39 -0600

    Parcial01 - add httpclient

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,7 +8,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/members").subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log("Complete the http request")
+    });
+  }
 }
 
