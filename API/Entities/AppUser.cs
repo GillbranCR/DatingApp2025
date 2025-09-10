745b45d197760c194ce1ccd7dd014c165e28305f
GillbranCR <gill.cadengo1@gmail.com>
2025-09-10 07:18:05 -0600

    Parcial02 - Add pasword hash salt

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -14,5 +14,9 @@ public class AppUser
 
     public required string Email { get; set; }
 
+    public required byte[] PasswordHash { get; set; }
+
+    public required byte[] PasswordSalt { get; set; }
+
 
 }
