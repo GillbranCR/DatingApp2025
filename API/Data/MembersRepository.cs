01aac6f8b2e7ef368eae0ac6935be7eac2cb0e93
GillbranCR <gill.cadengo1@gmail.com>
2025-10-20 07:07:19 -0600

    Parcial02 - Add  Member Repository skeleton Faltante

--- API/Data/MembersRepository.cs
+++ API/Data/MembersRepository.cs
@@ -2,10 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
 
 namespace API.Data;
 
-public class MembersRepository
+public class MembersRepository : IMembersRepository
 {
-    
+    public Task<Member?> GetMemberAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Member>> GetMembersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Photo>> GetPhotosAsync(string MemberId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Member member)
+    {
+        throw new NotImplementedException();
+    }
 }
