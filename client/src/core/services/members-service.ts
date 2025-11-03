cb851ff17eab879ea805343dc161c7b226ec7389
GillbranCR <gill.cadengo1@gmail.com>
2025-11-03 07:37:35 -0600

    Parcial03 - Add Photos content

--- client/src/core/services/members-service.ts
+++ client/src/core/services/members-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Member } from '../../types/member';
+import { Member, Photo } from '../../types/member';
 import { Observable } from 'rxjs';
 import { AccountService } from './account-service';
 
@@ -11,7 +11,6 @@ import { AccountService } from './account-service';
 export class MembersService {
   private http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
-  private accountService = inject(AccountService);
 
   getMember(id: string): Observable<Member> {
     return this.http.get<Member>(this.baseUrl + "members/" + id)
@@ -21,11 +20,7 @@ export class MembersService {
     return this.http.get<Member[]>(this.baseUrl + "members");
   }
 
-  private getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: "Bearer " + this.accountService.currentUser()?.token
-      })
-    };
+  getPhotos(id: string) {
+    return this.http.get<Photo[]>(`${this.baseUrl}members/${id}/photos`);
   }
 }
