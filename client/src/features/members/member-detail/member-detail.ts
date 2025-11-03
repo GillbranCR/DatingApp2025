4baf4c8d893b86910505df3e3c7b28e70aa08145
GillbranCR <gill.cadengo1@gmail.com>
2025-11-03 07:25:54 -0600

    Parcial03 - Add member resolver

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -3,7 +3,7 @@ import { MembersService } from '../../../core/services/members-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Member } from '../../../types/member';
-import { Observable, filter } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-member-detail',
@@ -15,11 +15,13 @@ export class MemberDetail implements OnInit {
   private membersService = inject(MembersService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  protected member$?: Observable<Member>;
+  protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>("Profile");
 
   ngOnInit(): void {
-    this.member$ = this.loadMember();
+    this.route.data.subscribe({
+        next: data => this.member.set(data["member"])
+    });
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(
@@ -29,15 +31,5 @@ export class MemberDetail implements OnInit {
         this.title.set(this.route.firstChild?.snapshot?.title);
       }
     });
-  }
-
-  loadMember(): Observable<Member> | undefined {
-    const id = this.route.snapshot.paramMap.get("id");
-
-    if (id) {
-      return this.membersService.getMember(id);
-    }
-
-    return;
   }
 }
